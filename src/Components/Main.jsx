99d6498fed6446432c21a835302e8639146e6317
Neel <neelpatel805804@gmail.com>
2023-01-31 00:32:02 +0530

    no changes

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,36 +1,53 @@
-import React from "react";
+import { useEffect, useReducer } from "react";
+import React, { useState } from "react";
 
 export const Main = () => {
+  ////////////////////////////////////////////////////////
+
+  
+
+  /////////////////////////////////////////////////////////////
+
+
+  
+
   return (
     <div className="main">
       <div className="main-header">
         <div>
-          <a className="main-tags" href="#">Pomodoro</a>
+          <a className="main-tags" href="#">
+            Pomodoro
+          </a>
         </div>
         <div>
-          <a className="main-tags" href="#">Short Break</a>
+          <a className="main-tags" href="#">
+            Short Break
+          </a>
         </div>
         <div>
-          <a className="main-tags" href="#">Long Break</a>
+          <a className="main-tags" href="#">
+            Long Break
+          </a>
         </div>
-       
       </div>
       <div className="timer">
-           <p>25:00</p>
-        </div>
+        <p>25:00</p>
+      </div>
 
-<div className="both-buttons">
+      <div className="both-buttons">
         <div className="start">
-            <button className="start-button " >
-<span >START</span>
-            </button>
+          <button className="start-button ">
+            <span >
+              START
+            </span>
+          </button>
         </div>
         <div className="start">
-            <button className="reset-button" >
-              <span >RESET</span>
-            </button>
+          <button className="reset-button">
+            <span >RESET</span>
+          </button>
         </div>
-        </div>
+      </div>
     </div>
   );
 };
