b86fdbcc24cd70038fde684febbfafcce4773684
Neel <neelpatel805804@gmail.com>
2023-02-08 22:56:02 +0530

    comments added in the code

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,37 +1,43 @@
 import React from "react";
 import { useState } from "react";
 
-
-
+// Todo component to create a to-do list
 export const Todo = () => {
-  
+  // State to store the list of tasks
   const [tasks, setTasks] = useState([]);
+
+  // State to store the value of the task input field
   const [taskInput, setTaskInput] = useState("");
+
+  // Function to handle changes in the task input field
   const handleTaskInput = (event) => {
-    
     setTaskInput(event.target.value); 
   };
 
+  // Function to add a task to the list
   const addTask = () => {
+    // Check if the task input is empty
     if(taskInput.length === 0){
-
+      // Show an alert if the task input is empty
       alert("Enter Task!!")
-      
-    }else{
+    } else {
+      // Add the task to the list
       setTasks([...tasks, taskInput]);
       setTaskInput("");
     }
-
-    
   };
 
+  // Function to delete a task from the list
   const deleteTask = (index) => {
+    // Filter the tasks to exclude the task to be deleted
     setTasks(tasks.filter((task, i) => i !== index));
   };
 
+  // Function to edit a task in the list
   const editTask = (index, updatedTask,event) => {
-
+    // Check if the updated task input is not empty
     if(updatedTask.length  != 0 )
+      // Map through the tasks list and replace the task to be edited
       setTasks(
         tasks.map((task, i) => {
           if (i === index) {
@@ -40,13 +46,11 @@ export const Todo = () => {
           return task;
         })
       );
-      else{
-        alert("Enter some input!!")
-      }
-    
+    else{
+      // Show an alert if the updated task input is empty
+      alert("Enter some input!!")
+    }
   };
-
-
 
 
   return (
